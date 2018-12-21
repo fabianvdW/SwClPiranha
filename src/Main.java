8d0424ba12a51ba7d0a8d2075de0db0df8130268
fabianvdW <fabianvonderwarth@gmail.com>
2018-12-21 19:43:15 +0100

    Fix to Logic

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,36 @@ import helpers.StringToGameStateConverter;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args){
-        BitBoardConstants.setSquareAttackDirectionSquareDestinationAttackLine();
-        GameState g= StringToGameStateConverter.readGameState(StringToGameStateConverter.STANDARD_GAME_STATE);
-        g.analyze();
-        System.out.println(g);
-        System.out.println("Plies: "+g.pliesPlayed);
-        System.out.println("Rounds: "+g.roundsPlayed);
-        System.out.println("GameState: "+g.gs);
-        while(g.gs==GameStatus.INGAME){
-            Object[] values = g.possibleMoves.values().toArray();
-            g= (GameState) values[(int)(values.length*Math.random())];
-            g.analyze();
-            System.out.println(g);
-            System.out.println("Plies: "+g.pliesPlayed);
-            System.out.println("Rounds: "+g.roundsPlayed);
-            System.out.println("GameState: "+g.gs);
+    public static void main(String[] args) {
+        for(int i=0;i<100000;i++){
+            GameState g= playGame(false);
+            if(g.gs!=GameStatus.DRAW){
+                System.out.println(g);
+            }
         }
+    }
+
+    public static GameState playGame(boolean verbose) {
+        BitBoardConstants.setSquareAttackDirectionSquareDestinationAttackLine();
+        GameState g = StringToGameStateConverter.readGameState(StringToGameStateConverter.STANDARD_GAME_STATE);
+        g.analyze();
+        if (verbose) {
+            System.out.println(g);
+            System.out.println("Plies: " + g.pliesPlayed);
+            System.out.println("Rounds: " + g.roundsPlayed);
+            System.out.println("GameState: " + g.gs);
+        }
+        while (g.gs == GameStatus.INGAME) {
+            Object[] values = g.possibleMoves.values().toArray();
+            g = (GameState) values[(int) (values.length * Math.random())];
+            g.analyze();
+            if (verbose) {
+                System.out.println(g);
+                System.out.println("Plies: " + g.pliesPlayed);
+                System.out.println("Rounds: " + g.roundsPlayed);
+                System.out.println("GameState: " + g.gs);
+            }
+        }
+        return g;
     }
 }
