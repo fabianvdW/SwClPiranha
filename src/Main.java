28cdf6db4ff2a4553ae30f5175f79d8df7aeb3c0
fabianvdW <fabianvonderwarth@gmail.com>
2018-12-21 17:34:34 +0100

    Added makeMove() and a method to calculate all possible moves

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import datastructures.BitBoard;
+import game.BitBoardConstants;
 import game.GameColor;
 import game.GameLogic;
 import game.GameState;
@@ -5,7 +7,10 @@ import helpers.StringToGameStateConverter;
 
 public class Main {
     public static void main(String[] args){
-        //GameState g= StringToGameStateConverter.readGameState(StringToGameStateConverter.STANDARD_GAME_STATE);
-        //System.out.println(g);
+        BitBoardConstants.setSquareAttackDirectionSquareDestinationAttackLine();
+        GameState g= StringToGameStateConverter.readGameState(StringToGameStateConverter.STANDARD_GAME_STATE);
+        System.out.println(g);
+        System.out.println(GameLogic.getPossibleMoves(g,GameColor.RED));
+        System.out.println(GameLogic.getPossibleMoves(g,GameColor.RED).size());
     }
 }
