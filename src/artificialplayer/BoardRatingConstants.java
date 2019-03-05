5d80657e959c745da7722adf0481c492ed13ac1c
fabianvdW <fabianvonderwarth@gmail.com>
2019-03-05 18:57:55 +0100

    Added Sufi, changed Features and GMRO, re-trained with GA, testing results

--- src/artificialplayer/BoardRatingConstants.java
+++ src/artificialplayer/BoardRatingConstants.java
@@ -3,7 +3,9 @@ package artificialplayer;
 import java.io.Serializable;
 
 public class BoardRatingConstants implements Serializable {
-    public static int size = 42;
+    static final long serialVersionUID = 42L;
+
+    public static int size = 21;
     Feature anzahlFische;
     Feature biggestSchwarmDurch16;
     Feature randFische;
@@ -15,12 +17,12 @@ public class BoardRatingConstants implements Serializable {
 
     public BoardRatingConstants(double[] consts) {
 
-        this.anzahlFische = new Feature(consts[0], consts[1], consts[2], consts[3], consts[4], consts[5]);
-        this.biggestSchwarmDurch16 = new Feature(consts[6], consts[7], consts[8], consts[9], consts[10], consts[11]);
-        this.randFische = new Feature(consts[12], consts[13], consts[14], consts[15], consts[16], consts[17]);
-        this.centerFische = new Feature(consts[18], consts[19], consts[20], consts[21], consts[22], consts[23]);
-        this.schwarmRatio = new Feature(consts[24], consts[25], consts[26], consts[27], consts[28], consts[29]);
-        this.abstandZuMitte = new Feature(consts[30], consts[31], consts[32], consts[33], consts[34], consts[35]);
-        this.schwarmAbstand = new Feature(consts[36], consts[37], consts[38], consts[39], consts[40], consts[41]);
+        this.anzahlFische = new Feature(consts[0], consts[1], consts[2]);
+        this.biggestSchwarmDurch16 = new Feature(consts[3], consts[4], consts[5]);
+        this.randFische = new Feature(consts[6], consts[7], consts[8]);
+        this.centerFische = new Feature(consts[9], consts[10], consts[11]);
+        this.schwarmRatio = new Feature(consts[12], consts[13], consts[14]);
+        this.abstandZuMitte = new Feature(consts[15], consts[16], consts[17]);
+        this.schwarmAbstand = new Feature(consts[18], consts[19], consts[20]);
     }
 }
--- src/artificialplayer/Feature.java
+++ src/artificialplayer/Feature.java
@@ -3,25 +3,19 @@ package artificialplayer;
 import java.io.Serializable;
 
 public class Feature implements Serializable {
+    static final long serialVersionUID = 42L;
+
     double a;
     double b;
     double c;
-    double d;
-    double e;
-    double f;
 
-    public Feature(double a, double b, double c, double d, double e, double f) {
+    public Feature(double a, double b, double c) {
         this.a = a;
         this.b = b;
         this.c = c;
-        this.d = d;
-        this.e = e;
-        this.f = f;
     }
 
     public double getOutput(double input, double phase) {
-        double xmb = input - b;
-        double xmbsquared = Math.pow(xmb, 2);
-        return a * xmbsquared + c * xmb + d * phase * (e * xmbsquared + f * xmb);
+        return a * input + phase * b * input + (1 - phase) * c * input;
     }
 }
